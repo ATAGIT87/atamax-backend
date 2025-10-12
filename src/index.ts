666befdca449d89b49eb55ad531005e249d9115f
ATAGIT87 <116157751+ATAGIT87@users.noreply.github.com>
2025-10-12 18:48:57 +0200

    apply cors

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
 import express from "express";
 import dotenv from "dotenv";
 import sgMail from "@sendgrid/mail";
+import cors from "cors";
 
 dotenv.config();
 
 const app = express();
+app.use(cors())
 const PORT = process.env.PORT || 3000;
 
 // Validate env variables
